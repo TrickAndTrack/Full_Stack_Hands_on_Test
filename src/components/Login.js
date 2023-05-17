10e6c8d2e9c5b81e03b3b4895aa4f8b86676959d
Trick & Track <73180409+TrickAndTrack@users.noreply.github.com>
2023-05-17 17:53:22 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,55 @@
 import axios from "axios";
 import {Navigate} from "react-router-dom";
-import {useState} from "react";
+import {useState,useRef } from "react";
 
 export const Login = () => {
     const [phoneNumber, setphoneNumber] = useState('');
     const [password, setPassword] = useState('');
     const [navigate, setNavigate] = useState(false);
+    const errRef = useRef();
+    const [errMsg, setErrMsg] = useState(null);
 
     const submit = async e => {
+
+
+
         e.preventDefault();
+        try {
 
-        const {data} = await axios.get('http://localhost:9009/api/v0/login', {
-          phoneNumber, password
-        }, {withCredentials: true});
+        // const {data} = await axios.get('http://localhost:9009/api/v0/login', {
+        //   phoneNumber, password
+        // }, {withCredentials: true});
 
-        console.log(data);
-        axios.defaults.headers.common['Authorization'] = `Bearer ${data['token']}`;
+        const response = await axios.post('http://localhost:9009/api/v0/login', {
+            phoneNumber: phoneNumber,
+            password: password
+          }, {withCredentials: true});
+
+        console.log(response.data);
+        //axios.defaults.headers.common['Authorization'] = `Bearer ${data['token']}`;
+        //localStorage.setItem('tocken',response.data.token)
 
         setNavigate(true);
     }
-
+    catch (err) {
+        if (!err?.response) {
+          setErrMsg('No Server Response');
+          return false;
+        } else if (err.response?.status === 401) {
+          setErrMsg('User UNAUTHORIZED');
+          return false;
+        } else {
+          setErrMsg('User Not Genrated ')
+          return false;
+        }
+    }
+    }
     if (navigate) {
         return <Navigate to="/"/>;
     }
 
     return <main className="form-signin">
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
         <form onSubmit={submit}>
             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
